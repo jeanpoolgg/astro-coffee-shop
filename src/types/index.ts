cbcef74af6912a347bda2498994508bba68b89fe
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-29 23:01:41 -0500

    feat(page): add Process page with WordPress REST integration and Zod validation

--- src/types/index.ts
+++ src/types/index.ts
@@ -27,3 +27,15 @@ export const BaseWPSchema = z.object({
         subtitle: z.string()
     })
 })
+
+const processSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string(),
+})
+
+export const ProcessPageSchema = BaseWPSchema.extend({
+    acf: z.object({
+        subtitle: z.string()
+    }).catchall(processSchema)
+})
