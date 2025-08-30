67137e06b5e6e7b83a46deb116a0d79a9523e067
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-30 09:01:27 -0500

    feat(lib): add centralized WordPress REST API client

---
+++ src/lib/wp.ts
@@ -0,0 +1,24 @@
+import { BaseWPSchema } from "@/types"
+
+const baseUrl = import.meta.env.WP_API_URL
+
+
+export const getPageNosotros = async () => {
+    const response = await fetch(`${baseUrl}/pages?slug=nosotros`)
+    if (!response.ok) throw new Error("Failed to fetch page about us")
+    const [data] = await response.json()
+    const dataAboutUs = BaseWPSchema.parse(data)
+    const {  title: { rendered: pageTitle }, acf: { subtitle }, featured_images: {medium_large: image}, content: {rendered:content} } = dataAboutUs
+    return { pageTitle, subtitle, image, content }
+}
+
+
+export const getPageProceso = async () => {
+    const response = await fetch(`${baseUrl}/pages?slug=proceso`)
+    if (!response.ok) throw new Error("Failed to fetch page process")
+    const [data] = await response.json()
+    const dataProcess = BaseWPSchema.parse(data)
+    const { title: { rendered: pageTitle }, acf, featured_images: {medium_large: image} , content: { rendered: content } } = dataProcess
+    return { pageTitle, acf, image, content }
+}
+
