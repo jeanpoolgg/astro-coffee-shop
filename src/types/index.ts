87bd981a86d67f16201ccdba8847cdd4ca9f1183
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-29 19:23:21 -0500

    feat(api): connect to WordPress REST using env variable

---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+import { z } from "astro:schema"
+
+const imageSchema = z.object({
+    url: z.string(),
+    width: z.number(),
+    height: z.number()
+})
+
+const featuredImagesSchema = z.object({
+    thumbnail: imageSchema,
+    medium: imageSchema,
+    medium_large: imageSchema,
+    large: imageSchema,
+    full: imageSchema
+})
+
+export const BaseWPSchema = z.object({
+    id: z.number(),
+    title: z.object({
+        rendered: z.string()
+    }),
+    content: z.object({
+        rendered: z.string()
+    }),
+    featured_images: featuredImagesSchema,
+    acf: z.object({
+        subtitle: z.string()
+    })
+})
